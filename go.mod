b352e6b6fd39fc7091b1853ec808b436b35ae3ed
Shubham Diwakar <shubham_1901ee58@iitp.ac.in>
2023-04-06 13:08:17 +0530

    Made tables using FrostDB

--- go.mod
+++ go.mod
@@ -5,42 +5,69 @@ go 1.20
 require github.com/segmentio/parquet-go v0.0.0-20230309140036-b6d0a6236da6
 
 require (
+	github.com/RoaringBitmap/roaring v0.9.4 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
-	github.com/apache/thrift v0.14.2 // indirect
+	github.com/apache/arrow/go/v10 v10.0.1 // indirect
+	github.com/apache/thrift v0.16.0 // indirect
 	github.com/aws/aws-sdk-go v1.44.217 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dennwc/varint v1.0.0 // indirect
+	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/efficientgo/tools/core v0.0.0-20220225185207-fe763185946b // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/btree v1.0.1 // indirect
+	github.com/google/flatbuffers v2.0.8+incompatible // indirect
 	github.com/grafana/regexp v0.0.0-20221122212121-6b5c0a4cb7fd // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/klauspost/asmfmt v1.3.2 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
+	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
+	github.com/mschoch/smat v0.2.0 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/polarsignals/frostdb v0.0.0-20230404074548-87066bb2a275 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/common/sigv4 v0.1.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/thanos-io/objstore v0.0.0-20220715165016-ce338803bc1e // indirect
+	github.com/tidwall/gjson v1.10.2 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/tinylru v1.1.0 // indirect
+	github.com/tidwall/wal v1.1.7 // indirect
 	github.com/xitongsys/parquet-go-source v0.0.0-20230312005205-fbbcdea5f512 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/goleak v1.2.1 // indirect
 	golang.org/x/exp v0.0.0-20230307190834-24139beb5833 // indirect
+	golang.org/x/mod v0.9.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/tools v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.29.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
@@ -48,12 +75,12 @@ require (
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/pierrec/lz4/v4 v4.1.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/prometheus/prometheus v0.43.0
 	github.com/segmentio/encoding v0.3.5 // indirect
 	github.com/xitongsys/parquet-go v1.6.2
--- hello.go
+++ hello.go
@@ -6,12 +6,15 @@ import (
 	"log"
 	"math"
 	"os"
+	"strings"
 
-	parquet1 "github.com/segmentio/parquet-go"
-	"github.com/xitongsys/parquet-go-source/local"
-	"github.com/xitongsys/parquet-go/parquet"
-	"github.com/xitongsys/parquet-go/reader"
-	"github.com/xitongsys/parquet-go/writer"
+	"github.com/apache/arrow/go/arrow/memory"
+	"github.com/apache/arrow/go/v10/arrow"
+	"github.com/polarsignals/frostdb"
+	schemapb "github.com/polarsignals/frostdb/gen/proto/go/frostdb/schema/v1alpha1"
+	"github.com/polarsignals/frostdb/query"
+	"github.com/polarsignals/frostdb/query/logicalplan"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/tsdb"
@@ -19,81 +22,17 @@ import (
 	tsdb_errors "github.com/prometheus/prometheus/tsdb/errors"
 )
 
-type TSDB struct {
-	Value float64 `parquet:"name=value"`
-	Time  int64   `parquet:"name=time"`
-	Label string  `parquet:"name=label"`
+type Data struct {
+	Value float64
+	Time  int64
+	LABEL Labels
+}
+type Labels []Label
+
+type Label struct {
+	Name, Value string
 }
 
-func SlicesEqual(a, b []*TSDB) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		} else {
-			fmt.Println(v, b[i])
-		}
-	}
-	return true
-}
-func generateParquet(block []*TSDB) error {
-	// using xitongsys library
-	log.Println("generating parquet file")
-	fw, err := local.NewLocalFileWriter("output.parquet")
-	fmt.Println(len(block))
-	if err != nil {
-		return err
-	}
-	//parameters: writer, type of struct, size
-	pw, err := writer.NewParquetWriter(fw, new(TSDB), int64(len(block)))
-	if err != nil {
-		return err
-	}
-	//compression type
-	pw.CompressionType = parquet.CompressionCodec_GZIP
-	defer fw.Close()
-	for _, d := range block {
-		if err = pw.Write(d); err != nil {
-			return err
-		}
-	}
-	if err = pw.WriteStop(); err != nil {
-		return err
-	}
-	u, err := readParquet(int64(len(block)))
-	for _, vv := range u {
-		fmt.Println(vv)
-	}
-	if err != nil {
-		return err
-	} else {
-		if SlicesEqual(u, block) {
-			fmt.Println("Writing done succesfully both same")
-		} else {
-			fmt.Println("Blocks unequal")
-		}
-	}
-	return nil
-}
-func readParquet(recordNumber int64) ([]*TSDB, error) {
-	fr, err := local.NewLocalFileReader("output.parquet")
-	if err != nil {
-		return nil, err
-	}
-	pr, err := reader.NewParquetReader(fr, new(TSDB), recordNumber)
-	if err != nil {
-		return nil, err
-	}
-	u := make([]*TSDB, recordNumber)
-	if err = pr.Read(&u); err != nil {
-		return nil, err
-	}
-	pr.ReadStop()
-	fr.Close()
-	return u, nil
-}
 func openBlock(path, blockID string) (*tsdb.DBReadOnly, tsdb.BlockReader, error) {
 	db, err := tsdb.OpenDBReadOnly(path, nil)
 	if err != nil {
@@ -119,6 +58,40 @@ func openBlock(path, blockID string) (*tsdb.DBReadOnly, tsdb.BlockReader, error)
 	}
 	return db, block, nil
 }
+func simpleSchema() proto.Message {
+	return &schemapb.Schema{
+		Name: "simple_schema",
+		Columns: []*schemapb.Column{{
+			Name: "value",
+			StorageLayout: &schemapb.StorageLayout{
+				Type: schemapb.StorageLayout_TYPE_DOUBLE,
+			},
+			Dynamic: false,
+		}, {
+			Name: "time",
+			StorageLayout: &schemapb.StorageLayout{
+				Type: schemapb.StorageLayout_TYPE_INT64,
+			},
+			Dynamic: false,
+		}, {
+			Name: "label",
+			StorageLayout: &schemapb.StorageLayout{
+				Type:     schemapb.StorageLayout_TYPE_STRING,
+				Encoding: schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
+				Nullable: true,
+			},
+			Dynamic: true,
+		}},
+		SortingColumns: []*schemapb.SortingColumn{{
+			Name:      "time",
+			Direction: schemapb.SortingColumn_DIRECTION_ASCENDING,
+		}, {
+			Name:       "label",
+			NullsFirst: true,
+			Direction:  schemapb.SortingColumn_DIRECTION_ASCENDING,
+		}},
+	}
+}
 func readTsdb(path string, blockID string) error {
 	db, _, err := openBlock(path, blockID)
 	if err != nil {
@@ -136,27 +109,51 @@ func readTsdb(path string, blockID string) error {
 
 	sset := q.Select(false, nil, labels.MustNewMatcher(labels.MatchRegexp, labels.MetricName, ".+"))
 
-	file, err := os.Create("../output_tsdb.parquet")
+	// Create a new column store
+	columnstore, err := frostdb.New()
 	if err != nil {
 		return err
 	}
-	var block []*TSDB
-	writer := parquet1.NewWriter(file)
+	defer columnstore.Close()
 
+	// Open up a database in the column store
+	database, err := columnstore.DB(context.Background(), "simple_db")
+	if err != nil {
+		return err
+	}
+	// Define our simple schema of labels and values
+	schema := simpleSchema()
+
+	// Create a table named simple in our database
+	table, err := database.Table(
+		"tsdb_table",
+		frostdb.NewTableConfig(schema),
+	)
+	if err != nil {
+		return err
+	}
 	for sset.Next() {
 		series := sset.At()
-		lbs := series.Labels().String()
+		lbs := series.Labels()
+		l := make([]Label, 0)
+		for _, v := range lbs {
+			r1 := strings.Split(fmt.Sprint(v), " ")
+			l = append(l, Label{Name: r1[0], Value: r1[1]})
+		}
+		//res, err := convertLabelsToString(m1)
+		//fmt.Println(jsonstr)
 		it := series.Iterator(nil)
 		for it.Next() == chunkenc.ValFloat {
 			ts, val := it.At()
-			tsdb := TSDB{
+			d := Data{
 				Value: val,
 				Time:  ts,
-				Label: lbs,
+				LABEL: l,
 			}
-			block = append(block, &tsdb)
-			//fmt.Println(tsdb)
-			if err := writer.Write(tsdb); err != nil {
+			_, err = table.Write(context.Background(), d)
+			if err != nil {
+				fmt.Println(d)
+				fmt.Println(err)
 				return err
 			}
 		}
@@ -164,26 +161,33 @@ func readTsdb(path string, blockID string) error {
 			return sset.Err()
 		}
 	}
-	generateParquet(block)
+	fmt.Println("Write succesfull")
+	// Create a new query engine to retrieve data and print the results
+	engine := query.NewEngine(memory.DefaultAllocator, database.TableProvider())
+	_ = engine.ScanTable("tsdb_table").
+		Project(logicalplan.DynCol("label")). // We don't know all dynamic columns at query time, but we want all of them to be returned.
+		Filter(
+			logicalplan.Col("label").Eq(logicalplan.Literal("job prometheus")),
+		).Execute(context.Background(), func(ctx context.Context, r arrow.Record) error {
+		fmt.Println(r)
+		return nil
+	})
 	return nil
 }
 func main() {
 
-	//fname := "./hello.go"
-	//path, err := filepath.Abs(fname)
 	path, err := os.Getwd()
-	//fmt.Println(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	blockId := "01GW1T7K3E9F9R361GDPVH8NZF"
-	//p := filepath.join(path, blockId)
-	//p := filepath.Join(path, blockId)
 	err = readTsdb(path, blockId)
 	if err == nil {
 		fmt.Println("Write Successful")
 	} else {
+		fmt.Println("error")
 		fmt.Println(err)
 	}
+
 }
